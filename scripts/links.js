8f7036ed9034af61e6991e5079bf11b1fa5320f1
arun sharma <hotdudehvfun@gmail.com>
2019-12-02 16:17:46 +0530

    new

--- scripts/links.js
+++ scripts/links.js
@@ -1,91 +1,237 @@
-let links =
-[
-    {
-        "text": "Election Commission",
-        "relPath": "election-commission.html"
-    },
-    {
-        "text": "Finance Commission",
-        "relPath": "Finance Commission.html"
-    },
-    {
-        "text": "Union Public Service Commission",
-        "relPath": "upsc.html"
-    },
-    {
-        "text": "State Public Service Commission",
-        "relPath": "spsc.html"
-    },
-    {
-        "text": "Advocate General Of India",
-        "relPath": "AG.html"
-    }, {
-        "text": "Advocate General of the State",
-        "relPath": "Advocate General of the State.html"
-    },
-    {
-        "text": "CAG",
-        "relPath": "CAG.html"
-    },
-    {
-        "text":"Land Reforms",
-        "relPath":"land reforms.html"
-    },
-    {
-        "text":"Anglo Mysore Wars",
-        "relPath":"Anglo Mysore Wars.htm"
-    },
-    {
-        "text":"Emergency Provisions",
-        "relPath":"Emergency Provisions.htm"
-    },
-    {
-        "text":"Writs Notes",
-        "relPath":"Writs.htm"
-    },
-    {
-        "text":"Polity:Chapter 1 Historical Background",
-        "relPath":"Polity chapter 1 historical background.htm"
-    },
-    {
-        "text":"Polity chapter 2 Making of Constitution",
-        "relPath":"Polity chapter 2 Making of Constitution.htm"
-    },
-    {
-        "text":"Polity:Chapter 3 Salient Features of Constitution",
-        "relPath":"Polity chapter 3 Salient Features of Constitution.htm"
-    },
-    {
-        "text":"Polity chapter 4 Preamble",
-        "relPath":"Polity chapter 4 Preamble.html"
-    },
-    {
-        "text":"Polity chapter 5 Union and Its Territory",
-        "relPath":"Polity chapter 5 Union and Its Territory.html"
-    },
-    {
-        "text":"Polity chapter 6 Fundamental Rights",
-        "relPath":"Polity chapter 6 Fundamental Rights.htm"
-    },
-    {
-        "text":"Polity chapter 8 DPSP",
-        "relPath":"Polity chapter 8 DPSP.html"
-    }
-    ,
-    {
-        "text":"Polity chapter 9 Fundamenta Duties",
-        "relPath":"Polity chapter 9 Fundamenta Duties.html"
-    }
-    ,
-    {
-        "text":"Polity chapter 10 Amendment to constitution",
-        "relPath":"Polity chapter 10 Amendment to constitution.html"
-    }
-    ,
-    {
-        "text":"Polity chapter 11 Basic Structure Cases",
-        "relPath":"Polity chapter 11 Basic Structure Cases.html"
-    }
-    
-    
-];
+let polityChapters =
+[{
+	"text": "Chapter 1 Historical Background",
+	"fileName": "Chapter 1.html"
+},
+{
+	"text": "Chapter 2 Making of the Constitution",
+	"fileName": "Chapter 2.html"
+},
+{
+	"text": "Chapter 3 Salient Features of the Constitution",
+	"fileName": "Chapter 3.html"
+},
+{
+	"text": "Chapter 4 Preamble of the Constitution",
+	"fileName": "Chapter 4.html"
+},
+{
+	"text": "Chapter 5 Union and its Territory",
+	"fileName": "Chapter 5.html"
+},
+{
+	"text": "Chapter 6 Citizenship",
+	"fileName": "Chapter 6.html"
+},
+{
+	"text": "Chapter 7 Fundamental Rights",
+	"fileName": "Chapter 7.html"
+},
+{
+	"text": "Chapter 8 Directive Principles of State Policy",
+	"fileName": "Chapter 8.html"
+},
+{
+	"text": "Chapter 9 Fundamental Duties",
+	"fileName": "Chapter 9.html"
+},
+{
+	"text": "Chapter 10 Amendment of the Constitution",
+	"fileName": "Chapter 10.html"
+},
+{
+	"text": "Chapter 11 Basic Structure of the Constitution",
+	"fileName": "Chapter 11.html"
+},
+{
+	"text": "Chapter 12 Parliamentary System",
+	"fileName": "Chapter 12.html"
+},
+{
+	"text": "Chapter 13 Federal System",
+	"fileName": "Chapter 13.html"
+},
+{
+	"text": "Chapter 14 Centre-State Relations",
+	"fileName": "Chapter 14.html"
+},
+{
+	"text": "Chapter 15 Inter-State Relations",
+	"fileName": "Chapter 15.html"
+},
+{
+	"text": "Chapter 16 Emergency Provisions",
+	"fileName": "Chapter 16.html"
+},
+{
+	"text": "Chapter 17 President",
+	"fileName": "Chapter 17.html"
+},
+{
+	"text": "Chapter 18 Vice-President",
+	"fileName": "Chapter 18.html"
+},
+{
+	"text": "Chapter 19 Prime Minister",
+	"fileName": "Chapter 19.html"
+},
+{
+	"text": "Chapter 20 Central Council of Ministers",
+	"fileName": "Chapter 20.html"
+},
+{
+	"text": "Chapter 21 Cabinet Committees",
+	"fileName": "Chapter 21.html"
+},
+{
+	"text": "Chapter 22 Parliament",
+	"fileName": "Chapter 22.html"
+},
+{
+	"text": "Chapter 23 Parliamentary Committees",
+	"fileName": "Chapter 23.html"
+},
+{
+	"text": "Chapter 24 Parliamentary Forums",
+	"fileName": "Chapter 24.html"
+},
+{
+	"text": "Chapter 25 Parliamentary Group",
+	"fileName": "Chapter 25.html"
+},
+{
+	"text": "Chapter 26 Supreme Court",
+	"fileName": "Chapter 26.html"
+},
+{
+	"text": "Chapter 27 Judicial Review",
+	"fileName": "Chapter 27.html"
+},
+{
+	"text": "Chapter 28 Judicial Activism",
+	"fileName": "Chapter 28.html"
+},
+{
+	"text": "Chapter 29 Public Interest Litigation",
+	"fileName": "Chapter 29.html"
+},
+{
+	"text": "Chapter 30 Governor",
+	"fileName": "Chapter 30.html"
+},
+{
+	"text": "Chapter 31 Chief Minister",
+	"fileName": "Chapter 31.html"
+},
+{
+	"text": "Chapter 32 State Council of Ministers",
+	"fileName": "Chapter 32.html"
+},
+{
+	"text": "Chapter 33 State Legislature",
+	"fileName": "Chapter 33.html"
+},
+{
+	"text": "Chapter 34 High Court",
+	"fileName": "Chapter 34.html"
+},
+{
+	"text": "Chapter 35 Subordinate Courts",
+	"fileName": "Chapter 35.html"
+},
+{
+	"text": "Chapter 36 Special Status of Jammu & Kashmir",
+	"fileName": "Chapter 36.html"
+},
+{
+	"text": "Chapter 37 Special Provisions for Some States",
+	"fileName": "Chapter 37.html"
+},
+{
+	"text": "Chapter 38 Panchayati Raj",
+	"fileName": "Chapter 38.html"
+},
+{
+	"text": "Chapter 39 Municipalities",
+	"fileName": "Chapter 39.html"
+},
+{
+	"text": "Chapter 40 Union Territories",
+	"fileName": "Chapter 40.html"
+},
+{
+	"text": "Chapter 41 Scheduled and Tribal Areas",
+	"fileName": "Chapter 41.html"
+},
+{
+	"text": "Chapter 42 Election Commission",
+	"fileName": "Chapter 42.html"
+},
+{
+	"text": "Chapter 43 Union Public Service Commission",
+	"fileName": "Chapter 43.html"
+},
+{
+	"text": "Chapter 44 State Public Service Commission",
+	"fileName": "Chapter 44.html"
+},
+{
+	"text": "Chapter 45 Finance Commission",
+	"fileName": "Chapter 45.html"
+},
+{
+	"text": "Chapter 46 National Commission for SCs",
+	"fileName": "Chapter 46.html"
+},
+{
+	"text": "Chapter 47 National Commission for STs",
+	"fileName": "Chapter 47.html"
+},
+{
+	"text": "Chapter 48 Special Officer for Linguistic Minorities",
+	"fileName": "Chapter 48.html"
+},
+{
+	"text": "Chapter 49 Comptroller and Auditor General",
+	"fileName": "Chapter 49.html"
+},
+{
+	"text": "Chapter 50 Attorney General",
+	"fileName": "Chapter 50.html"
+},
+{
+	"text": "Chapter 51 Advocate General",
+	"fileName": "Chapter 51.html"
+},
+{
+	"text": "Chapter 52 NITI Aayog",
+	"fileName": "Chapter 52.html"
+},
+{
+	"text": "Chapter 53 National Human Rights Commission",
+	"fileName": "Chapter 53.html"
+},
+{
+	"text": "Chapter 54 State Human Rights Commission",
+	"fileName": "Chapter 54.html"
+},
+{
+	"text": "Chapter 55 Central Information Commission",
+	"fileName": "Chapter 55.html"
+},
+{
+	"text": "Chapter 56 State Information Commission",
+	"fileName": "Chapter 56.html"
+},
+{
+	"text": "Chapter 57 Central Vigilance Commission",
+	"fileName": "Chapter 57.html"
+},
+{
+	"text": "Chapter 58 Central Bureau of Investigation",
+	"fileName": "Chapter 58.html"
+},
+{
+	"text": "Chapter 59 Lokpal and Lokayuktas ",
+	"fileName": "Chapter 59.html"
+}]
